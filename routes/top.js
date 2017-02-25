1f5df693f454461a7eb3a810eb0349016fd95817
RajuC <raju.chittimallea@gmail.com>
2017-02-25 04:04:56 -0800

    removed unwanted files

--- routes/top.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Contact page. */
-router.get('/top', function(req, res, next) {
-  res.render('contact')});
-
-module.exports = router;
